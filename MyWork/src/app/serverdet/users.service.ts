16cc1f13205f149e4d92f185218b0cf666782f73
aswani <aswani@logicalsteps.net>
2018-06-13 16:43:39 +0530

    updates

--- MyWork/src/app/serverdet/users.service.ts
+++ MyWork/src/app/serverdet/users.service.ts
@@ -2,36 +2,42 @@ import { Injectable } from '@angular/core';
 
 export const user =  [
   {
+    "id" : 1,
     "name": "aswani",
     "email": "aswani@gmail.com",
     "age": "25",
     "place": "kozhikode",
     "password": "aswani123"
   },{
+    "id" : 2,
     "name": "vishnu",
     "email": "vishnu@gmail.com",
     "age": "27",
     "place": "ernakulam",
     "password": "vishnu123"
   },{
+    "id" : 3,
     "name": "devika",
     "email": "devika@gmail.com",
     "age": "25",
     "place": "ernakulam",
     "password": "devika123"
   },{
+    "id" : 4,
     "name": "divya",
     "email": "divya@gmail.com",
     "age": "27",
     "place": "ernakulam",
     "password": "divya123"
   },{
+    "id" : 5,
     "name": "deepa",
     "email": "deepa@gmail.com",
     "age": "28",
     "place": "ernakulam",
     "password": "deepa123"
   },{
+    "id" : 6,
     "name": "rukma",
     "email": "rukma@gmail.com",
     "age": "27",
@@ -40,7 +46,6 @@ export const user =  [
   }];
 
 @Injectable()
-
 export class UsersService {
 
   constructor() { }
